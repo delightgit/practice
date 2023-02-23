46b1de9f225325d7d12a46bac65a9976581d18b3
Delight Lee <98308370+delightgit@users.noreply.github.com>
2023-02-22 16:50:53 -0800

    first commit

---
+++ PracticeDemo/src/cst438/ApplicationDemo.java
@@ -0,0 +1,19 @@
+package cst438;
+
+public class ApplicationDemo {
+	public static void main(String[] args) {
+		String[] shopList = new String[] {
+				"Apples",
+				"Bananas",
+				"Corn",
+				"Dog Food",
+				"Cereal", 
+				"Bread", 
+				"Eggs",
+				"Milk"
+		};
+		for (String item: shopList) {
+			System.out.println(item);
+		}	
+       }
+}
---
+++ PracticeDemo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author broth
+ *
+ */
+module PracticeDemo {
+}
